10412d33b5c40b6bba22f44e52da9ea8de85644a
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2024-10-14 20:22:40 +0700

    Reactivity fundamentals: ref vs reactive

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,17 @@
 <script setup>
-import { ref, computed } from "vue";
-const header = ref("Shopping List App");
-const characterCount = computed(() => {
-  return newItem.value.length;
-});
-const items = ref([
-  { id: 1, label: "10 party hats", purchased: true, hightPriority: false },
-  { id: 2, label: "2 board games", purchased: true, hightPriority: false },
-  { id: 3, label: "20 cups", purchased: false, hightPriority: true },
-]);
-
-const reversedItems = computed(() => {
-  return [...items.value].reverse();
-});
-
-const newItem = ref("");
-const newItemPriority = ref("low");
-const newItemHighPriority = ref(false);
-const editing = ref(false);
-const saveItem = () => {
-  items.value.push({
-    id: items.value.length + 1,
-    label: newItem.value,
-    hightPriority: newItemHighPriority.value,
-  });
-  newItem.value = "";
-  newItemHighPriority.value = false;
-};
-const doEdit = (e) => {
-  editing.value = e;
-  newItem.value = "";
-};
-const togglePurchased = (item) => {
-  item.purchased = !item.purchased;
+import { ref, reactive } from "vue";
+const msg = ref("Hello World");
+const state = reactive({ count: 0 });
+const increment = () => {
+  state.count++;
 };
 </script>
 
 <template>
-  <div class="header">
-    <h1>{{ header }}</h1>
-    <button v-if="editing" class="btn" @click="doEdit(false)">Cancel</button>
-    <button v-else class="btn btn-primary" @click="doEdit(true)">
-      Add Item
-    </button>
-  </div>
-  <form class="add-item-form" v-on:submit.prevent="saveItem" v-if="editing">
-    <input type="text" placeholder="Add an Item" v-model="newItem" />
-
-    <label>
-      <input type="checkbox" v-model="newItemHighPriority" /> Hight Priority
-    </label>
-    <button class="btn btn-primary" v-bind:disabled="newItem.length < 3">
-      Save Item
-    </button>
-  </form>
-  <p class="counter">{{ characterCount }}</p>
-  <ul>
-    <li
-      v-for="({ id, label, purchased, hightPriority }, index) in reversedItems"
-      @click="togglePurchased(items[index])"
-      :key="id"
-      :class="{
-        strikeout: purchased,
-        priority: hightPriority,
-        'static-class': true,
-      }"
-    >
-      {{ label }}
-    </li>
-  </ul>
-  <p v-if="!items.length">Nothing to see here</p>
+  <h1>{{ msg }}</h1>
+  <h2>{{ state.count }}</h2>
+  <button @click="state.count++">Increment Count</button>
+  <button @click="increment">Increment Count</button>
+  <br />
+  <input v-model="msg" />
 </template>
